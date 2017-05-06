8238a98bc797b5c22722b551db9a67796d249d69
masterj <ronchaos@gmail.com>
2017-05-06 23:46:41 +0900

    update GameScreen.java file

--- mrnom2/app/src/main/java/com/badlogic/framework/androidgames/mrnom/GameScreen.java
+++ mrnom2/app/src/main/java/com/badlogic/framework/androidgames/mrnom/GameScreen.java
@@ -141,5 +141,123 @@ public class GameScreen extends Screen {
           scroe.length() * 20 / 2, g.getHeight() - 42);
     }
 
+    private void drawWorld(World world) {
+      Graphics g = game.getGraphics();
+      Snake snake = world.snake;
+      SnakePart head = snake.parts.get(0);
+      Stain stain = wolrd.stain;
+
+      Pixmap stainPixmap = null;
+      if (stain.type == Stain.TYPE_1)
+        stainPixmap = Assets.stainq;
+      if (stain.type == Stain.TYPE_2)
+        stainPixmap = Assets.stain2;
+      if (stain.type == Stain.TYPE_3)
+        stainPixmap = Assets.stain3;
+      int x = stain.x * 32;
+      int y = stain.y * 32;
+      g.drawPixmap(stainPixmap, x, y);
+
+      int len = snake.parts.size();
+      for (int idx = 0; idx < len; idx++) {
+        SnakePart part = snake.parts.get(idx);
+        x = part.x * 32;
+        y = part.y * 32;
+        g.drawPixmap(Assets.tail, x, y);
+      }
+
+      Pixmap headPixmap = null;
+      if (snake.direction == Snake.UP)
+        headPixmap = Assets.headUp;
+      if (snake.direction == Snake.LEFT)
+        headPixmap = Assets.headLeft;
+      if (snake.direction == Snake.DOWN)
+        headPixmap = Assets.headDown;
+      if (snake.direction == Snake.RIGHT)
+        headPixmap = Assets.headRight;
+     
+      x = head.x * 32 + 16;
+      y = head.y * 32 + 16;
+      g.drawPixmap(headPixmap, x - headPixmap.getWidth() / 2, y - headPixmap.getHeight() / 2)
+
+    }
+
+    private void drawReadyUI() {
+      Graphics g = game.getGraphics();
+
+      g.drawPixmap(Assets.ready, 47, 100);
+      g.drawLin(0, 416, 480, 416, Color.BLACK);
+    }
+
+    private void drawRunningUI() {
+      Graphics g = game.getGraphics();
+
+      g.drawPixmap(Assets.buttons, 0, 0, 64, 128, 64, 64);
+      g.drawLine(0, 416, 480, 416, Color.BLACK);
+      g.drawPixmap(Assets.buttons, 0, 416, 64, 64, 64, 64);
+      g.drawPixmap(Assets.buttons, 256, 416, 64, 64, 64, 64);
+    }
+
+    private void drawPausedUI() {
+
+      Graphic g = game.getGraphics();
+
+      g.drawPixmap(Asssets.pause, 80, 100);
+      g.drawLine(0, 416, 480, 416, Color.BLACK);
+    }
+
+    private void drawGameOverUI() {
+ 
+      Graphics g = game.getGraphics();
+
+      g.drawPixmap(Assets.gameOver, 62, 100);
+      g.drawPixmap(Assets.buttons, 128, 200, 0, 128, 64, 64);
+      g.drawLinep(0, 416, 480, 416, Color.BLACK);
+    }
+
+    public void drawText(Graphics g, String line, int x, int y) {
+      int len = line.length();
+      for (int idx = 0; idx < len; idx++) {
+        char character = line.charAt(idx);
+
+        if (character == ' ') {
+          x += 20;
+          continue;
+        }
+
+        int srcX = 0;
+        int srcWidth = 0;
+        if (character == '.') {
+          srcX = 200;
+          srcWidth = 10;
+        } else {
+          srcX = (character - '0') * 20;
+          srcWidth = 20;
+        }
+
+        g.drawPixmap(Assets.numbers, x, y, srcX, 0, srcWidth, 32);
+        x += srcWidth;
+      }
+
+    }
+
+    @Override
+    public void pause() {
+      if (state = GameState.Running)
+        state = GameState.Paused;
+
+      if (world.gameOver) {
+        Settings.AddScore(world.score);
+        Settings.save(game.getFileIO());
+      }
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Ovveride
+    public void dispose() {
+    }
 
 }
